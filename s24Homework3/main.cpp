2f671a4d78b7f99d1975c159a80938325f87eeb2
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-05-05 20:19:05 -0700

    Initial Commit

---
+++ s24Homework3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  s24Homework3
+//
+//  Created by Cameron Maiden on 5/5/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

e7911adf728ac47fd84448e8b5db13dd71be80b5
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-05-05 21:01:49 -0700

    Initial Commit *Added appropriate class definitions as required of the spec *Added my test cases to the main function

---
+++ s24Homework3/investment.cpp
@@ -0,0 +1,66 @@
+//
+//  investment.cpp
+//  s24Homework3
+//
+//  Created by Cameron Maiden on 5/5/24.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Your declarations and implementations would go here
+class Investment
+{
+public:
+    
+private:
+    
+};
+
+class Painting
+{
+public:
+    
+private:
+};
+
+class Stock
+{
+public:
+    
+private:
+};
+
+class House
+{
+public:
+    
+private:
+};
+
+void display(const Investment* inv)
+{
+//    cout << inv->name();
+//    if (inv->fungible())
+//        cout << " (fungible)";
+//    cout << ": " << inv->description() << " bought for $" << inv->purchasePrice() << endl;
+}
+
+int main()
+{
+//    Investment* portfolio[4];
+//    portfolio[0] = new Painting("Salvator Mundi", 450300000);
+//      // Stock holdings have a name, value, and ticker symbol
+//    portfolio[1] = new Stock("Nvidia", 50000, "NVDA");
+//    portfolio[2] = new Stock("Costco", 10000, "COST");
+//    portfolio[3] = new House("4 Privet Drive", 670000);
+//
+//    for (int k = 0; k < 4; k++)
+//        display(portfolio[k]);
+//
+//      // Clean up the investments before exiting
+//    cout << "Cleaning up." << endl;
+//    for (int k = 0; k < 4; k++)
+//        delete portfolio[k];
+}

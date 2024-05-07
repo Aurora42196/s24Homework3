f96ed7395f0eacc6dbb212785b9d4c64b06e5589
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-05-07 11:00:44 -0700

    *Changed the fungible function from pure virtual to virtual

--- s24Homework3/investment.cpp
+++ s24Homework3/investment.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 ///////////////////////////////////////////////////////////////////////////
-//  Function declarations
+//  Class/Function declarations
 ///////////////////////////////////////////////////////////////////////////
 ///
 class Investment
@@ -18,14 +18,14 @@ class Investment
 public:
     Investment(string name, int pp);
     virtual ~Investment();
+    virtual string description() const = 0;
+    virtual bool fungible() const;
     
     // Accessors
     string name() const;
     int purchasePrice() const;
     
 private:
-    virtual string description() const = 0;
-    virtual bool fungible() const = 0;
     string m_name;
     int m_price;
 };
@@ -35,11 +35,9 @@ class Painting : public Investment
 public:
     Painting(string name, int pp);
     virtual ~Painting();
-    virtual string description();
+    virtual string description() const;
     
 private:
-    virtual string description() const;
-    virtual bool fungible() const;
 };
 
 class Stock : public Investment
@@ -47,11 +45,10 @@ class Stock : public Investment
 public:
     Stock(string name, int pp, string ticker);
     virtual ~Stock();
-    virtual string description();
-
-private:
     virtual string description() const;
     virtual bool fungible() const;
+
+private:
     string m_ticker;
 };
 
@@ -60,17 +57,16 @@ class House : public Investment
 public:
     House(string address, int pp);
     virtual ~House();
-    virtual string description();
+    virtual string description() const;
 
 private:
-    virtual string description() const;
-    virtual bool fungible() const;
 };
 
 ///////////////////////////////////////////////////////////////////////////
 //  Function implementations
 ///////////////////////////////////////////////////////////////////////////
 ///
+// Investment functions
 Investment::Investment(string name, int pp)
  :m_name(name), m_price(pp)
 {}
@@ -88,22 +84,65 @@ int Investment::purchasePrice() const
     return m_price;
 }
 
-Painting::Painting(string name, int pp)
- : Investment(name, pp)
-{}
+bool Investment::fungible() const
+{
+    return false;
+}
+
+// Painting functions
+Painting::Painting(string name, int price)
+ : Investment(name, price)
+{
+    
+}
 
 Painting::~Painting()
-{}
+{
+    cout << "Destroying " << name() << ", a painting" << endl;
+}
 
 string Painting::description() const
 {
     return "painting";
 }
 
-bool Painting::fungible() const
+
+// Stock functions
+Stock::Stock(string name, int pp, string ticker)
+ : Investment(name, pp), m_ticker(ticker)
+{}
+
+Stock::~Stock()
 {
-    return false;
+    cout << "Destroying " << name() << ", a stock holding" << endl;
 }
+
+string Stock::description() const
+{
+    return "stock trading as " + m_ticker;
+}
+
+bool Stock::fungible() const
+{
+    return true;
+}
+
+// House functions
+House::House(string address, int pp)
+ :Investment(address, pp)
+{}
+
+House::~House()
+{
+    cout << "Desroying the house " << name() << endl;
+}
+
+string House::description() const
+{
+    return "house";
+}
+
+
 
 
 ///////////////////////////////////////////////////////////////////////////
@@ -112,26 +151,27 @@ bool Painting::fungible() const
 
 void display(const Investment* inv)
 {
-//    cout << inv->name();
-//    if (inv->fungible())
-//        cout << " (fungible)";
-//    cout << ": " << inv->description() << " bought for $" << inv->purchasePrice() << endl;
+    cout << inv->name();
+    if (inv->fungible())
+        cout << " (fungible)";
+    cout << ": " << inv->description() << " bought for $" << inv->purchasePrice() << endl;
 }
 
 int main()
 {
+//    Investment* ip = new Investment("name", 0);
     Investment* portfolio[4];
     portfolio[0] = new Painting("Salvator Mundi", 450300000);
-//      // Stock holdings have a name, value, and ticker symbol
-//    portfolio[1] = new Stock("Nvidia", 50000, "NVDA");
-//    portfolio[2] = new Stock("Costco", 10000, "COST");
-//    portfolio[3] = new House("4 Privet Drive", 670000);
-//
-//    for (int k = 0; k < 4; k++)
-//        display(portfolio[k]);
-//
-//      // Clean up the investments before exiting
-//    cout << "Cleaning up." << endl;
-//    for (int k = 0; k < 4; k++)
-//        delete portfolio[k];
+      // Stock holdings have a name, value, and ticker symbol
+    portfolio[1] = new Stock("Nvidia", 50000, "NVDA");
+    portfolio[2] = new Stock("Costco", 10000, "COST");
+    portfolio[3] = new House("4 Privet Drive", 670000);
+
+    for (int k = 0; k < 4; k++)
+        display(portfolio[k]);
+
+      // Clean up the investments before exiting
+    cout << "Cleaning up." << endl;
+    for (int k = 0; k < 4; k++)
+        delete portfolio[k];
 }
